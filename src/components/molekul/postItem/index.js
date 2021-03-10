84bb18f7a3a4a79d889bbf84a09e0f2d0ddf97a3
adhitiad <68454354+adhitiad@users.noreply.github.com>
2021-03-10 10:16:36 +0700

    new

--- src/components/molekul/postItem/index.js
+++ src/components/molekul/postItem/index.js
@@ -1,26 +1,20 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { RegisterBg } from "../../../assets";
+// import { RegisterBg } from "../../../assets";
 import { Button, Gap } from "../../atoms";
 import "./post.scss";
 
-function PostItem() {
+function PostItem(props) {
   const history = useHistory();
   return (
     <div className="post-item">
-      <img className="img-thumb" src={RegisterBg} alt="post" />
+      <img className="img-thumb" src={props.image} alt="post" />
       <div className="content-detail">
-        <p className="title"> Title</p>
-        <p className="author">Auhor - date</p>
-        <p className="body">
-          Proident commodo aliquip nisi ea consequat adipisicing ullamco
-          occaecat excepteur. Labore sint consequat id nostrud laboris. Pariatur
-          ad elit aliquip ipsum in reprehenderit occaecat reprehenderit occaecat
-          exercitation aliquip velit. Duis ea nisi incididunt sunt nulla
-          exercitation reprehenderit officia id occaecat nulla culpa mollit
-          nulla. Adipisicing occaecat eiusmod Lorem in cupidatat irure qui
-          consequat ex adipisicing.
+        <p className="title">{props.title}</p>
+        <p className="author">
+          {props.name} - {props.date}
         </p>
+        <p className="body">{props.body}</p>
         <Gap height={10} />
         <Button nama="Detail" onClick={() => history.push("/detail-post")} />
       </div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Gap, PostItem } from "./../../components";
+import Axios from "axios";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
 
 function Home() {
+  const [dataBlogs, setDataBlog] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:5000/posts")
+      .then((result) => {
+        console.log("berhasil terhubung ke server dengan data :", result.data);
+        const resApi = result.data;
+        setDataBlog(resApi.data);
+      })
+      .catch((err) => {
+        console.log("error : ", err);
+      });
+  }, []);
   const history = useHistory();
   return (
     <div className="home-wrap">
@@ -12,10 +25,11 @@ function Home() {
       </div>
       <Gap height={20} />
       <div className="post-i">
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
+        {dataBlogs.map((blog) => {
+          return (
+            <PostItem key={blog._id} title={blog.title} body={blog.body} />
+          );
+        })}
       </div>
       <div className="paginasi">
         <Button nama="Prev" />
